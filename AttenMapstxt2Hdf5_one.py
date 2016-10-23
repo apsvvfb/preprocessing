dd91eeb1b036cc0dc8b5bee6033126d16b53c622
apsvvfb <apsvvfb@apsvvfbdeMacBook-Air.local>
2016-10-23 18:01:23 +0900

    home

--- AttenMapstxt2Hdf5_one.py
+++ AttenMapstxt2Hdf5_one.py
@@ -5,28 +5,22 @@ import h5py
 import os
 #imgpath=sys.argv[1]
 
-numArea=5
 pathTxt="AttenMaps/"
-outfile="AttenMapsHdf5/train.hdf5"
-#ImgIDs="ImgIDs_ori.txt"
-ImgIDs="trainIDs.txt"
+outfile="AttenMapsHdf5/AreaAll.hdf5"
+IDfiles=["trainIDs.txt","testIDs.txt"]
+for tid, ImgIDs in enumerate(IDfiles):
+  f=open(ImgIDs,'r')
+  lines=f.readlines()
+  f.close()
+  attenImgs  = np.ones((len(lines),196))/196
+  attenprobs = np.zeros((len(lines),196))
 
-f=open(ImgIDs,'r')
-lines=f.readlines()
-f.close()
-attenprobs=np.zeros((len(lines),numArea))
-aid=-1
-attenImg=np.ones((1,196))/196
-for ix,line in enumerate(lines):
-  outID=line.strip()
-  print outID
-  files=glob.glob("%s/%s_*.txt" %(pathTxt,outID))  
-  if len(files) >= numArea:
-     aid=aid+1
-     attens=np.zeros((1,196*(numArea+1)))
-     attens[0:,0:196]=attenImg
+  for ix,line in enumerate(lines):
+     outID=line.strip()
+     print outID
+     files=glob.glob("%s/%s_*.txt" %(pathTxt,outID))  
+     attens=np.zeros((len(files),196))
      for fid,filename in enumerate(files):
-	if fid < numArea:
 	   f=open(filename,'r')
 	   nums=f.read()
 	   f.close()
@@ -34,8 +28,14 @@ for ix,line in enumerate(lines):
 	   for i in range(0,196):
 	       atten[i]=nums.split(' ')[i]
 	   atten=(atten-min(atten))/(max(atten)-min(atten))
-	   attens[0:,(fid+1)*196:(fid+2)*196]=atten
-     attenprobs[aid]=attens
+	   attens[fid]=atten
+     attenprobs[ix]=np.sum(attens, axis=0)/len(files)
+  final=np.hstack((attenImgs,attenprobs))
+  if tid == 0:
+	trainmatrix=final 
+  else:
+	testmatrix=final
+
 with h5py.File(outfile, 'w') as hf:
-  hf.create_dataset('areaprobs', data=attenprobs)
-#sys.exit()
+   hf.create_dataset('trainareaprobs', data=trainmatrix)
+   hf.create_dataset('testareaprobs', data=testmatrix)
